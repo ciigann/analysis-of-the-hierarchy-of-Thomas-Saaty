a6edbfa27c8fae6126fcaff337eecee018d6c9d1
C_gan <maxvetrov2004@yandex.ru>
2023-10-26 18:41:01 +0500

    Доработал программу, добавил проверку корректности ввода для количества критериев и веса для пары, добавил комментарии к программе и выделил заголовок "Весовые коэффициенты:"

--- анализа иерархии Томаса Саати.py
+++ анализа иерархии Томаса Саати.py
@@ -1,3 +1,6 @@
+from colorama import Style
+
+
 def get_weights(num_criteria):
     # Создаем матрицу размером num_criteria x num_criteria
     matrix = [[0 for _ in range(num_criteria)] for _ in range(num_criteria)]
@@ -12,7 +15,18 @@ def get_weights(num_criteria):
                 # Если уже ввели вес для пары (j, i), то используем его
                 matrix[i][j] = 1 / matrix[j][i]
             else:
-                weight = float(input(f"Введите вес для пары ({i}, {j}): "))
+                weight = 0
+                while weight == 0:
+                    try:  # что бы проверить является ли введенная строка числом попробуем преобразовать ее в float и будем ловить исключение
+                        weight = float(input(f"Введите вес для пары ({i}, {j}): "))
+                        if weight < 0:  # проверим является ли введенное число положительным
+                            weight = 0
+                            print('\033[31m',
+                                  'вес для пары должно быть задан положительным числом!!!' + Style.RESET_ALL)
+                    except ValueError:  # ловим исключение при ошибке
+                        print('\033[31m', 'вес для пары должно быть задан числом!!!' + Style.RESET_ALL)
+                        num_criteria = 0
+                        pass
                 matrix[i][j] = weight
 
     # Вычисляем весовые коэффициенты
@@ -31,10 +45,20 @@ def get_weights(num_criteria):
 
 
 # Вводим количество критериев
-num_criteria = int(input("Введите количество критериев: "))
+num_criteria = 0
+while num_criteria == 0:
+    try:  # что бы проверить является ли введенная строка числом попробуем преобразовать ее в int и будем ловить исключение
+        num_criteria = int(input("Введите количество критериев: "))
+        if num_criteria < 0:  # проверим является ли введенное число положительным
+            num_criteria = 0
+            print('\033[31m', 'Количество критериев должно быть задано положительным числом!!!' + Style.RESET_ALL)
+    except ValueError:  # ловим исключение при ошибке
+        print('\033[31m', 'Количество критериев должно быть задано целым числом!!!' + Style.RESET_ALL)
+        num_criteria = 0
+        pass
 weights = get_weights(num_criteria)
 
 # Выводим весовые коэффициенты
-print("Весовые коэффициенты:")
+print("\033[1m" "\nВесовые коэффициенты:" + Style.RESET_ALL)
 for weight in weights:
     print(f"Весовой коэффициент: {weight:.2f}")
